3d6b0c787a9f14eea3ee6a9516f9bdb95467a32f
Gabriel Alexander Alvarado <gabrielalvarado@meta.com>
2024-06-28 09:42:09 -0700

    sign up and log in pages functionality complete, tied to firebase. Also added log out button to homepage

--- src/login/register.jsx
+++ src/login/register.jsx
@@ -1,89 +1,89 @@
-import React, { useState } from "react";
-import { Navigate, Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/authContext/index";
-import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
+// import React, { useState } from "react";
+// import { Navigate, Link, useNavigate } from "react-router-dom";
+// import { useAuth } from "../contexts/authContext/index";
+// import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
 
-const Register = () => {
-  const navigate = useNavigate();
+// const Register = () => {
+//   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setconfirmPassword] = useState("");
-  const [isRegistering, setIsRegistering] = useState(false);
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setconfirmPassword] = useState("");
+//   const [isRegistering, setIsRegistering] = useState(false);
 
-  const { userLoggedIn } = useAuth();
+//   const { userLoggedIn } = useAuth();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    if (!isRegistering) {
-      setIsRegistering(true);
-      await doCreateUserWithEmailAndPassword(email, password);
-    }
-  };
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     if (!isRegistering) {
+//       setIsRegistering(true);
+//       await doCreateUserWithEmailAndPassword(email, password);
+//     }
+//   };
 
-  return (
-    <>
-      {userLoggedIn && <Navigate to={"/home"} replace={true} />}
+//   return (
+//     <>
+//       {userLoggedIn && <Navigate to={"/home"} replace={true} />}
 
-      <main>
-        <div>
-          <div>
-            <h3>Create a New Account</h3>
-          </div>
-          <form onSubmit={onSubmit}>
-            <div>
-              <label>Email</label>
-              <input
-                type="email"
-                autoComplete="email"
-                required
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
+//       <main>
+//         <div>
+//           <div>
+//             <h3>Create a New Account</h3>
+//           </div>
+//           <form onSubmit={onSubmit}>
+//             <div>
+//               <label>Email</label>
+//               <input
+//                 type="email"
+//                 autoComplete="email"
+//                 required
+//                 value={email}
+//                 onChange={(e) => {
+//                   setEmail(e.target.value);
+//                 }}
+//               />
+//             </div>
 
-            <div>
-              <label>Password</label>
-              <input
-                disabled={isRegistering}
-                type="password"
-                autoComplete="new-password"
-                required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
+//             <div>
+//               <label>Password</label>
+//               <input
+//                 disabled={isRegistering}
+//                 type="password"
+//                 autoComplete="new-password"
+//                 required
+//                 value={password}
+//                 onChange={(e) => {
+//                   setPassword(e.target.value);
+//                 }}
+//               />
+//             </div>
 
-            <div>
-              <label>Confirm Password</label>
-              <input
-                disabled={isRegistering}
-                type="password"
-                autoComplete="off"
-                required
-                value={confirmPassword}
-                onChange={(e) => {
-                  setconfirmPassword(e.target.value);
-                }}
-              />
-            </div>
+//             <div>
+//               <label>Confirm Password</label>
+//               <input
+//                 disabled={isRegistering}
+//                 type="password"
+//                 autoComplete="off"
+//                 required
+//                 value={confirmPassword}
+//                 onChange={(e) => {
+//                   setconfirmPassword(e.target.value);
+//                 }}
+//               />
+//             </div>
 
-            <button type="submit" disabled={isRegistering}>
-              {isRegistering ? "Signing Up..." : "Sign Up"}
-            </button>
-            <div>
-              Already have an account? {"   "}
-              <Link to={"/"}>Log In</Link>
-            </div>
-          </form>
-        </div>
-      </main>
-    </>
-  );
-};
+//             <button type="submit" disabled={isRegistering}>
+//               {isRegistering ? "Signing Up..." : "Sign Up"}
+//             </button>
+//             <div>
+//               Already have an account? {"   "}
+//               <Link to={"/"}>Log In</Link>
+//             </div>
+//           </form>
+//         </div>
+//       </main>
+//     </>
+//   );
+// };
 
-export default Register;
+// export default Register;
