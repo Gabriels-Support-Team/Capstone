b554f6c948fedaf343069de998f9c1a1d1b71957
Capn05 <gxalvarado2013@gmail.com>
2024-06-28 16:59:03 -0700

    Reapply "basic prisma skeleton"
    This reverts commit f9af69f35ea49725b31c5e3e1b7f4d87e17e2283.

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+import userRoutes from "./Routes/users.js";
+
+app.listen(PORT, () => {
+  console.log("server is running");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!");
+});
+
+app.use("/users", userRoutes);
