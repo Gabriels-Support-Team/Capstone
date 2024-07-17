40a1a06762161e85fca37e8cf90a735d028f7495
Capn05 <gxalvarado2013@gmail.com>
2024-07-17 16:59:01 -0700

    Ml Recommendatinos are now specific to the user that is logged in and can be retrived in the front end through a api call

--- scripts/FetchMLRecs.js
+++ scripts/FetchMLRecs.js
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+const router = express.Router();
 const prisma = new PrismaClient();
 async function fetchRatings() {
   return prisma.userMovies
@@ -13,7 +16,7 @@ async function fetchRatings() {
       },
     })
     .then((ratings) => {
-      ratings.map((r) => ({
+      return ratings.map((r) => ({
         userID: r.userId,
         itemID: r.movie.movieId,
         rating: r.rating / 2,
@@ -22,14 +25,41 @@ async function fetchRatings() {
     });
 }
 import { spawn } from "child_process";
-function processRatingsWithPython() {
-  fetchRatings().then((ratings) => {
-    const pythonProcess = spawn("python3", ["../svd_movie_ratings.py"]);
-    pythonProcess.stdin.write(JSON.stringify(ratings));
-    pythonProcess.stdin.end();
-    pythonProcess.stdout.on("data", (data) => {
-      console.log(`Python Output: ${data}`);
+function processRatingsWithPython(req, res) {
+  const userId = req.query.userId;
+  if (!userId) {
+    return res.status(400).send("UserId is required");
+  }
+  fetchRatings()
+    .then((ratings) => {
+      const pythonProcess = spawn("python3", [
+        "svd_movie_ratings.py",
+        userId.toString(),
+      ]);
+
+      pythonProcess.stdin.write(JSON.stringify(ratings));
+      pythonProcess.stdin.end();
+      let outputData = "";
+      pythonProcess.stdout.on("data", (data) => {
+        outputData += data.toString();
+      });
+      pythonProcess.stderr.on("data", (data) => {
+        console.error(`Python Error: ${data.toString()}`);
+      });
+      pythonProcess.on("close", (code) => {
+        if (code !== 0) {
+          console.log(`Python script exited with code ${code}`);
+          return res.status(500).send("Failed to process ratings");
+        }
+        console.log("Python Output:", outputData);
+        res.send(outputData);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred while fetching ratings");
     });
-  });
 }
-processRatingsWithPython();
+
+router.get("/fetchRecs", processRatingsWithPython);
+export default router;
